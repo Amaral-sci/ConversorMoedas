bbe3d2775d869ef2e0c7da1a794cc33d8de11894
Jonathan <Jonathan_kazi@icloud.com>
2025-12-01 23:27:53 -0300

    one add

---
+++ ConversorMoedas/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  ConversorMoedas
+//
+//  Created by Jonathan Amaral on 01/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .padding()
+            Text("Hello, world!")
+
+            Button("PRECIONE PARA INICIAR") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConversorMoedas/ConversorMoedasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversorMoedasApp.swift
+//  ConversorMoedas
+//
+//  Created by Jonathan Amaral on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ConversorMoedasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
