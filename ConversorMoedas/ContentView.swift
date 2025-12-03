ff7407d3dcb181c50f8332ed4e5ad9f85a996e9a
Jonathan <Jonathan_kazi@icloud.com>
2025-12-02 23:11:59 -0300

    Day 2

--- ConversorMoedas/ContentView.swift
+++ ConversorMoedas/ContentView.swift
@@ -9,21 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-                .padding()
-            Text("Hello, world!")
-
-            Button("PRECIONE PARA INICIAR") {
-                
+        //ZStack HStack VStack = camadas de imagem
+        ZStack{
+            //imagem de fundo
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                Text("Conversão de Moeda")
+                    .font(Font.largeTitle)
+                    .foregroundStyle(.white)
+                HStack{
+                    VStack{
+                        HStack{ Image(.silverpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 50)
+                            Text(" Silver ")
+                                .font(Font.headline)
+                                .foregroundStyle(.white)
+                        }
+                        Text("text")
+                    }
+                    Image(systemName: "equal")
+                        .font(Font.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    VStack{
+                        HStack{
+                            Text(" Gold ")
+                                .font(Font.headline)
+                                .foregroundStyle(.white)
+                            Image(.goldpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 50)
+                        }
+                        Text( "text")
+                    }
+                        
+                }
+                Spacer()
+                Image(systemName: "info.circle.fill")
+                    .font(Font.largeTitle)
+                    .foregroundStyle(.white)
             }
         }
-        .padding()
     }
 }
-
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
